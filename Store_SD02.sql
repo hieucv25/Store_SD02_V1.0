f5b81256e794f50b63747c96e1ea794fe596dd1e
hieucv25 <117982824+hieucv25@users.noreply.github.com>
2024-02-24 11:15:08 +0700

    Update Store_SD02.sql

--- Store_SD02.sql
+++ Store_SD02.sql
@@ -430,3 +430,9 @@ alter table SanPhamChiTiet drop column MauSac
 alter table SanPhamChiTiet add MauSac int
 alter table SanPhamChiTiet add constraint FK_MauSac foreign key (MauSac) references MauSac(id)
 alter table SanPhamChiTiet add SoLuong int
+
+-- xoá khoá ngoại id sản phẩm của bảng chi tiết đơn hàng thay bằng id chi tiết sản phẩm
+Alter table SanPhamChiTiet drop Constraint FK_SPTT
+Alter table SanPhamChiTiet drop column idSanPham
+Alter table SanPhamChiTiet add idSanPhamChiTiet int
+Alter table SanPhamChiTiet Add Constraint FK_SPCT Foreign Key (idSanPhamChiTiet) references SanPhamChiTiet(Id)
